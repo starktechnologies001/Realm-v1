62e7ce803f4be02c0bba94645fb77ef65d052cf3
anubhaw-rgb <88982571+anubhaw-rgb@users.noreply.github.com>
2026-06-13 10:05:39 +0530

    chat screen fixeddd

--- social-map-app/src/pages/Chat.jsx
+++ social-map-app/src/pages/Chat.jsx
@@ -4130,7 +4130,7 @@ function ChatRoom({ currentUser, targetUser, onBack, allChats, replyToMessage: i
                 }
 
                 .chat-room-container {
-                    position: fixed; top: 0; left: 0; right: 0; bottom: 0;
+                    position: absolute; top: 0; left: 0; right: 0; bottom: 0;
                     width: 100%;
                     height: 100%; /* Allow resize with keyboard */
                     z-index: 10000;
